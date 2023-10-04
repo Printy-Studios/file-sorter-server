2da8be7174ff49b395992b8b901fb8dddb17b05f
JorensM <jorensmerenjanu@gmail.com>
2023-10-04 16:21:15 +0300

    feat(endpoints): add error handler for api/sort endpoint

--- src/index.ts
+++ src/index.ts
@@ -31,29 +31,35 @@ app.get('/', (req: Request, res: Response) => {
 })
 //
 app.post('/api/sort', async (req: Request, res: Response, next) => {
-
-    const {
-        conditions,
-        action
-    } = req.body;
-
-    logger.log('Sorting cloud storage folder', 'api/sort');
-    if(!googleClient) {
-        logger.log('Could not authorize Google account');
-        res.send('Could not authorize Google account').status(401);
-        next();
+    try {
+        const {
+            conditions,
+            action
+        } = req.body;
+    
+        logger.log('Sorting cloud storage folder', 'api/sort');
+        if(!googleClient) {
+            logger.log('Could not authorize Google account');
+            res.send('Could not authorize Google account').status(401);
+            next();
+        }
+    
+        const drive = driveInstance(googleClient)
+    
+        const driveSorter = new GoogleDriveSorter(drive, SORTER_CONFIG);
+    
+    
+        const files = await driveSorter.sort(conditions, action)
+    
+        logger.log('Sorted files')
+    
+        res.json(files)
+    } catch (e) {
+        res.json({
+            message: e.message
+        }).status(500)
     }
-
-    const drive = driveInstance(googleClient)
-
-    const driveSorter = new GoogleDriveSorter(drive, SORTER_CONFIG);
-
-
-    const files = await driveSorter.sort(conditions, action)
-
-    logger.log('Sorted files')
-
-    res.json(files)
+    
 })
 
 app.listen(PORT, () => {
