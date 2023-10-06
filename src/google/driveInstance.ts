d6cf1e4cf2a59429f755574af950392e97e23ac9
JorensM <jorensmerenjanu@gmail.com>
2023-10-06 16:55:38 +0300

    feat(driveInstance()): add type guard

--- src/google/driveInstance.ts
+++ src/google/driveInstance.ts
@@ -1,5 +1,10 @@
 import { google } from 'googleapis';
+import { JSONClient, GoogleAuth } from 'google-auth-library/build/src/auth/googleauth';
+import { OAuth2Client } from 'google-auth-library';
 
-export default function driveInstance(authClient) {
+export default function driveInstance(authClient: OAuth2Client | GoogleAuth<JSONClient>) {
+    if(!(authClient instanceof OAuth2Client) && !(authClient instanceof GoogleAuth)) {
+        throw new Error('authClient argument must be an instance of AuthClient')
+    }
     return google.drive({ version: 'v3', auth: authClient })
 }
